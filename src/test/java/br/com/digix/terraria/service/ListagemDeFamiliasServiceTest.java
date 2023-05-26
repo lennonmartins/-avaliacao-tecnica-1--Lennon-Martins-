36e36ab71a6dad221a979b818322423dabf00934
Lennon <lennonmcarlos@gmail.com>
2023-05-25 20:23:32 -0400

    Tentativa de teste para servicoDeValidacao e ordenagemDeFamilias

--- src/test/java/br/com/digix/terraria/service/ListagemDeFamiliasServiceTest.java
+++ src/test/java/br/com/digix/terraria/service/ListagemDeFamiliasServiceTest.java
@@ -2,27 +2,61 @@ package br.com.digix.terraria.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.digix.terraria.builders.FamiliaBuilder;
+import br.com.digix.terraria.dominio.Conjuge;
 import br.com.digix.terraria.dominio.Familia;
+import br.com.digix.terraria.dominio.Responsavel;
 import br.com.digix.terraria.dominio.exceptions.ConjugeInvalidException;
 import br.com.digix.terraria.dominio.exceptions.DependentesInvalidException;
 import br.com.digix.terraria.dominio.exceptions.NomeInvalidException;
 import br.com.digix.terraria.dominio.exceptions.ResponsavelInvalidException;
+import br.com.digix.terraria.dtos.requests.FamiliaRequestDTO;
+import br.com.digix.terraria.mappers.FamiliaMapper;
+import br.com.digix.terraria.repository.ConjugeRepository;
+import br.com.digix.terraria.repository.FamiliaRepository;
+import br.com.digix.terraria.repository.ResponsavelRepository;
 
 @SpringBootTest
 public class ListagemDeFamiliasServiceTest {
 
     ValidacaoDeCriterioService servico = new ValidacaoDeCriterioService();
-    
+
+    @Autowired
+    private FamiliaRepository familiaRepository;
+
+    @Autowired
+    private ResponsavelRepository responsavelRepository;
+
+    @Autowired
+    private ConjugeRepository conjugeRepository;
+
+    @Autowired
+    private FamiliaMapper familiaMapper;
+
+    @BeforeEach
+    @AfterEach
+    public void deleteDados() {
+        familiaRepository.deleteAll();
+        responsavelRepository.deleteAll();
+        conjugeRepository.deleteAll();
+    };
+
     @Test
-    void deve_retornar_uma_lista_de_familias() throws ResponsavelInvalidException, ConjugeInvalidException, DependentesInvalidException, NomeInvalidException{
+    void deve_retornar_uma_lista_de_familias() throws ResponsavelInvalidException, ConjugeInvalidException,
+            DependentesInvalidException, NomeInvalidException {
         int quantidadeDeFamiliaEsperada = 2;
         Familia familia1 = new FamiliaBuilder().criar();
         Familia familia2 = new FamiliaBuilder().criar();
-        
+
         ListagemDeFamiliasService lista = new ListagemDeFamiliasService();
         lista.adicionarFamilia(familia1);
         lista.adicionarFamilia(familia2);
@@ -31,22 +65,20 @@ public class ListagemDeFamiliasServiceTest {
     }
 
     @Test
-    void deve_retornar_uma_lista_de_familias_ordenadas_pela_pontuacao() throws ResponsavelInvalidException, ConjugeInvalidException, DependentesInvalidException, NomeInvalidException{
-        int primeiraPosicao = 0;
-        int ultimaPosicao = 5;
-        int maiorPontuacao = 15; 
-        int menorPontuacao = 0;
+    void deve_retornar_uma_lista_de_familias_ordenadas_pela_pontuacao() throws ResponsavelInvalidException,
+            ConjugeInvalidException, DependentesInvalidException, NomeInvalidException {
+        int pontuacaoEsperada = 5;
         ListagemDeFamiliasService lista = new ListagemDeFamiliasService();
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(5).criar());
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(10).criar());
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(maiorPontuacao).criar());
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(3).criar());
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(menorPontuacao).criar());
-        lista.adicionarFamilia(new FamiliaBuilder().comPontos(13).criar());
+        long idResponsavel = responsavelRepository.save(new Responsavel("Lennon")).getId();
+        long idDoConjuge = conjugeRepository.save(new Conjuge("Maria")).getId();
+        double rendaMensal = 850;
+        FamiliaRequestDTO familiaRequestDTO = new FamiliaRequestDTO(idDoConjuge, idResponsavel, null, rendaMensal);
+        familiaRepository.save(familiaMapper.familiaRequestParaFamilia(familiaRequestDTO));
+        List<Familia> familias = new ArrayList<>();
+        familias.add(familiaRepository.save(familiaMapper.familiaRequestParaFamilia(familiaRequestDTO)));
 
         lista.ordernarFamiliasPelaPontuacao();
 
-        assertThat(lista.listarFamilias().get(primeiraPosicao).getPontuacao()).isEqualTo(maiorPontuacao);
-        assertThat(lista.listarFamilias().get(ultimaPosicao).getPontuacao()).isEqualTo(menorPontuacao);
+        assertThat(familias.get(0).getPontuacao()).isEqualTo(pontuacaoEsperada);
     }
 }
--- src/test/java/br/com/digix/terraria/service/ServicoDeValidacaoServiceTest.java
+++ src/test/java/br/com/digix/terraria/service/ServicoDeValidacaoServiceTest.java
@@ -6,20 +6,29 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.digix.terraria.builders.DependenteBuilder;
 import br.com.digix.terraria.builders.FamiliaBuilder;
+import br.com.digix.terraria.dominio.Conjuge;
 import br.com.digix.terraria.dominio.Dependente;
 import br.com.digix.terraria.dominio.Familia;
+import br.com.digix.terraria.dominio.Responsavel;
 import br.com.digix.terraria.dominio.exceptions.ConjugeInvalidException;
 import br.com.digix.terraria.dominio.exceptions.DataDeNascimentoInvalid;
 import br.com.digix.terraria.dominio.exceptions.DataNascimentoDependenteInvalid;
 import br.com.digix.terraria.dominio.exceptions.DependentesInvalidException;
 import br.com.digix.terraria.dominio.exceptions.NomeInvalidException;
 import br.com.digix.terraria.dominio.exceptions.ResponsavelInvalidException;
+import br.com.digix.terraria.dtos.requests.FamiliaRequestDTO;
+import br.com.digix.terraria.mappers.FamiliaMapper;
+import br.com.digix.terraria.repository.ConjugeRepository;
+import br.com.digix.terraria.repository.FamiliaRepository;
+import br.com.digix.terraria.repository.ResponsavelRepository;
 
 @SpringBootTest
 public class ServicoDeValidacaoServiceTest {
@@ -27,9 +36,30 @@ public class ServicoDeValidacaoServiceTest {
   @Autowired
   private ValidacaoDeCriterioService servicoDeValidacao;
 
+  @Autowired
+  private FamiliaRepository familiaRepository;
+
+  @Autowired
+  private ResponsavelRepository responsavelRepository;
+
+  @Autowired
+  private ConjugeRepository conjugeRepository;
+
+  @Autowired
+  private FamiliaMapper familiaMapper;
+  
+  @BeforeEach
+	@AfterEach
+	public void deleteDados() {
+    familiaRepository.deleteAll();
+    responsavelRepository.deleteAll();
+		conjugeRepository.deleteAll();
+	};
+
   @Test
   void deve_validar_se_atende_aos_criterios()
-      throws ResponsavelInvalidException, ConjugeInvalidException, DependentesInvalidException, NomeInvalidException, DataDeNascimentoInvalid, DataNascimentoDependenteInvalid {
+      throws ResponsavelInvalidException, ConjugeInvalidException, DependentesInvalidException, NomeInvalidException,
+      DataDeNascimentoInvalid, DataNascimentoDependenteInvalid {
     int pontuacaoEsperada = 7;
     Dependente dependente = new DependenteBuilder().comDataDeNascimento(LocalDate.of(2010, 7, 19)).criar();
     Familia familia = new FamiliaBuilder().comDependente(dependente).criar();
@@ -40,5 +70,20 @@ public class ServicoDeValidacaoServiceTest {
     servicoDeValidacao.validarOsCriteriosAtendidos(listaDeFamilia);
 
     assertThat(familia.getPontuacao()).isEqualTo(pontuacaoEsperada);
+  }
+
+  @Test
+  void deve_pontuar_a_familia_de_acordo_com_o_criterio()
+      throws NomeInvalidException, ResponsavelInvalidException, ConjugeInvalidException, DependentesInvalidException {
+    int pontuacaoEsperada = 5;
+    long idResponsavel = responsavelRepository.save(new Responsavel("Lennon")).getId();
+    long idDoConjuge = conjugeRepository.save(new Conjuge("Maria")).getId();
+    double rendaMensal = 850;
+    FamiliaRequestDTO familiaRequestDTO = new FamiliaRequestDTO(idDoConjuge, idResponsavel, null, rendaMensal);
+    familiaRepository.save(familiaMapper.familiaRequestParaFamilia(familiaRequestDTO));
+
+    List<Familia> familias = servicoDeValidacao.pontuarFamiliaPelosCriteriosAtendidos();
+
+    assertThat(familias.get(0).getPontuacao()).isEqualTo(pontuacaoEsperada);
   }
 }
